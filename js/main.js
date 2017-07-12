fee08e57745f94b5fe76bc2ef10ead681cf8206e
Anna Shuster <a_chocolate@yahoo.com>
2017-07-12 01:56:15 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -7,16 +7,36 @@
 //console.log("User flipped " + cardFour);
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped" + " "+ cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped" + " "+ cardTwo);
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]){
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again!");
+var checkForMatch = function(){
+  if (cardsInPlay[0] === cardsInPlay[1])
+   alert("You found a match!");
+  else {
+   alert("Sorry, try again!");
   }
+
+ }
+var flipcard = function(cardId){
+
+
+//var cardOne = cards[0];
+//cardsInPlay.push(cardOne);
+//console.log("User flipped" + " "+ cardOne);
+//var cardTwo = cards[2];
+//cardsInPlay.push(cardTwo);
+//console.log("User flipped" + " "+ cardTwo);
+console.log("User flipped" + " " +cards[cardId]);
+cardsInPlay.push(cards[cardId]);
 }
+flipcard(0);
+flipcard(2);
+checkForMatch();
+
+//if (cardsInPlay.length === 2) {
+ // if (cardsInPlay[0] === cardsInPlay[1]){
+  //  alert("You found a match!");
+  //} else {
+  //  alert("Sorry, try again!");
+  //}
+//}
+
+
