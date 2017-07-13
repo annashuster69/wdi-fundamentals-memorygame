f652984b9537423fdb5f1537ae818f5c41c7947a
Anna Shuster <a_chocolate@yahoo.com>
2017-07-12 22:40:28 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -5,7 +5,22 @@
 //var cardFour = "king";
 //console.log("User flipped " + cardOne);
 //console.log("User flipped " + cardFour);
-var cards = ["queen", "queen", "king", "king"];
+//var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{ rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png"},
+{ rank: "queen",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png"},
+{ rank: "king",
+  suit: "hearts",
+  cardImage:"images/king-of-hearts.png"},
+{ rank: "king",
+  suit: "diamonds",
+  cardImage: "images/king-of-diamonds.png"},
+
+  ];
 var cardsInPlay = [];
 var checkForMatch = function(){
   if (cardsInPlay[0] === cardsInPlay[1])
@@ -13,6 +28,7 @@ var checkForMatch = function(){
   else {
    alert("Sorry, try again!");
   }
+
 
  }
 var flipcard = function(cardId){
@@ -24,8 +40,12 @@ var flipcard = function(cardId){
 //var cardTwo = cards[2];
 //cardsInPlay.push(cardTwo);
 //console.log("User flipped" + " "+ cardTwo);
-console.log("User flipped" + " " +cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped" + " " +cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
+
+cardsInPlay.push(cards[cardId].rank);
+//checkForMatch();
 }
 flipcard(0);
 flipcard(2);
